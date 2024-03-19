deb3eb16a9d419082d9e4c0b26e315f3639731f4
Tiago-Mendes32 <tiago.mendes.goes@gmail.com>
2024-03-19 13:27:16 -0300

    Create userRepository

--- src/main/java/legacy_med_project/repositories/UserRepository.java
+++ src/main/java/legacy_med_project/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package legacy_med_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import legacy_med_project.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	UserDetails findByLogin(String login);
 }
