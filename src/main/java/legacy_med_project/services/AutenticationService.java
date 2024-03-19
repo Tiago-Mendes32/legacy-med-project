e4f4167be957a9bbfabacd58e5ed6365f53b9290
Tiago-Mendes32 <tiago.mendes.goes@gmail.com>
2024-03-19 13:26:48 -0300

    Create autentication service

--- src/main/java/legacy_med_project/services/AutenticationService.java
+++ src/main/java/legacy_med_project/services/AutenticationService.java
@@ -1,8 +1,22 @@
 package legacy_med_project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import legacy_med_project.repositories.UserRepository;
+
 @Service
-public class AutenticationService {
+public class AutenticationService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return repository.findByLogin(username);
+	}
 
 }
